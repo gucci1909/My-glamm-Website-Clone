ea6ae46628c4a4f5549e9a9009a42be358efa943
gucci1909 <umangarora0134@gmail.com>
2022-08-27 15:21:30 +0530

    merge

---
+++ script/index.js
@@ -0,0 +1,7 @@
+import navbar from "../component/navbar.js";
+let nav = navbar();
+document.getElementById("navbar").innerHTML=nav;
+
+import footer from "../component/footer.js";
+let fot = footer();
+document.getElementById("footer").innerHTML = fot;
---
+++ script/index1.js
@@ -0,0 +1,61 @@
+let gotosignup = ()=>{
+    window.location.href="./signup.html"
+}
+
+function gotocart(){
+    window.location.href="./Cart.html";
+}
+
+let image =[
+    "./image/1.jpg",
+    "./image/2.jpg",
+    "./image/3.jpg",
+    "./image/4.jpg"
+]
+let address;
+let index=0;
+let slideshow = ()=>{
+    let cont = document.getElementById('slide');
+
+    address = setInterval(()=>{
+        if(index == image.length){
+            index=0;
+        }
+        cont.innerHTML="";
+        let img = document.createElement("img");
+        img.src = image[index];
+        cont.append(img);
+        index++;
+    },3000);
+};
+slideshow();
+
+let previousSilde = ()=>{
+    let cont = document.getElementById('slide');
+    index--;
+    if(index<0){
+        index=image.length-1;
+    }
+    cont.innerHTML="";
+    let img = document.createElement("img");
+    img.src = image[index];
+    cont.append(img);
+}
+
+let nextSlide = ()=>{
+    // clearInterval(address);
+    
+    let cont = document.getElementById('slide');
+    if(index == image.length){
+        index=0;
+    }
+    cont.innerHTML="";
+    let img = document.createElement("img");
+    img.src = image[index];
+    cont.append(img);
+    index++;
+}
+
+let data = JSON.parse(localStorage.getItem("user"));
+
+document.getElementById("user-name").innerText = data.value;
